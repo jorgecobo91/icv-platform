b14d0dfee162c298c90eb9109d5bbafab5e8cdf0
Jorge Vergara <coboandres@live.cl>
2026-03-03 20:59:47 -0300

    export materials service

--- backend/src/materials/materials.module.ts
+++ backend/src/materials/materials.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Material } from './material.entity';
-import { Stock } from '../stock/stock.entity';
-import { PurchaseOrder } from '../purchase-orders/purchase-order.entity';
 import { MaterialsService } from './materials.service';
 import { MaterialsController } from './materials.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Material,
-      Stock,
-      PurchaseOrder,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([Material])],
   controllers: [MaterialsController],
   providers: [MaterialsService],
+  exports: [MaterialsService], // 🔥 ESTA LÍNEA ES LA CLAVE
 })
 export class MaterialsModule {}
